8eb2c80c254017dac891cae68b5420a0df6f7ca0
jahid-hasan-babu <jahidhasanbabu7821@gmail.com>
2025-05-25 21:39:50 +0600

    input of c++ done

---
+++ module1/input.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x;
+    cin >> x;
+    int ascii = 'a';
+    cout << x << endl << ascii << endl;
+    cout  << (char)x;
+
+    return 0;
+}
