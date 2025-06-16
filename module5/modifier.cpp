4e13a5fc58e56e704e4f0105845bb32ffb01d1f1
jahid-hasan-babu <jahidhasanbabu7821@gmail.com>
2025-06-16 21:33:17 +0600

    string modifier done

---
+++ module5/modifier.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s = "Hello Jahid";
+    string s2 = "Hi";
+    // s += s2; 
+    // s.append(s2); 
+    s.push_back('A');
+    cout << s << endl;
+    return 0;
+}
