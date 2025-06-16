99f207c91f1bdfd2384add9bf73cfd4ed11494dc
jahid-hasan-babu <jahidhasanbabu7821@gmail.com>
2025-06-16 22:33:13 +0600

    string constructor done

---
+++ module6/string_constructor.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    // string s = "Hello"; 
+    // string s("Hello"); 
+    // string s("Hello world", 4); 
+    // string s = "Hello world";
+    // string t(s, 4); 
+
+    // cout << s << endl;
+    // cout << t << endl;
+
+    string s (10, 'B');
+    cout << s << endl;
+
+    return 0;
+}
