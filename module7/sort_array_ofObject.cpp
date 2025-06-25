dbc784071e4e5e754ee5a2b5934f9f5c645a23ec
jahid-hasan-babu <jahidhasanbabu7821@gmail.com>
2025-06-25 23:29:45 +0600

    multiple sorting done

--- module7/sort_array_ofObject.cpp
+++ module7/sort_array_ofObject.cpp
@@ -9,11 +9,26 @@ class Student{
 };
 
 bool cmp(Student l, Student r){
-    // if(l.marks < r.marks){
+    // if(l.marks <= r.marks){
     //     return true;
     // }else{
     //     return false;
     // }
+
+    if(l.marks > r.marks){
+        return true;
+    }else if(l.marks <  r.marks)
+    {
+        return false;
+    }else{
+        if(l.roll <  r.roll){
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 
     return l.marks < r.marks;
 }
